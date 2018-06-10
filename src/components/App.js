7226e35f59a7d7bf1e2ff6b70988f4568ed7ab67
parksy1775 <parsy1775@naver.com>
2018-06-10 17:54:31 +0900

    init project  ( skip detail discription )

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Nav from './P_Navigation';
+//import logo from './logo.svg';
+
+
+class App extends Component {
+  render() {
+    return (
+        <Nav/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/L_BoxOffice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class L_BoxOffice extends Component {
+  render() {
+    return (
+      <div>
+        Box office page
+      </div>
+    );
+  }
+}
+
+export default L_BoxOffice;
